e874ff666100fe6a297057258c4501e437191634
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-07-14 20:53:13 +0200

    simply fantastic

--- src/components/administration/component/ad-giochi/ad-giochi.component.ts
+++ src/components/administration/component/ad-giochi/ad-giochi.component.ts
@@ -1,23 +1,41 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { AdministrationService } from '../../../../services/administration.service';
+import { GiochiService } from '../../../../services/giochi.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ad-giochi',
   standalone: true,
-  imports: [NgIf, MatMenuModule, NgFor],
+  imports: [NgIf, MatMenuModule, NgFor, ReactiveFormsModule],
   templateUrl: './ad-giochi.component.html',
   styleUrl: './ad-giochi.component.scss'
 })
-export class AdGiochiComponent implements OnInit {
+export class AdGiochiComponent implements OnInit, AfterContentChecked {
   innerWidth: number = 0;
-  items: string[] = ["Supervisiona giochi", "Modifica un gioco", "Cancella un gioco"];
-  action:string = "";
-  constructor(private administrationService: AdministrationService) { }
+  items: string[] = ["Visualizza", "Modifica", "Elimina"];
+  action: string = "";
+  filterForms: FormGroup = new FormGroup({});
+  page: number = 0;
+  size: number = 1;
+  orderBy: string = 'id';
+  sortOrder: string = 'ASC';
+  giochi: any = null;
+  orders: { value: string, label: string }[] = [{ value: "id", label: "id" }, { value: "difficolta", label: "difficolta" }, { value: "createdAt", label: "Data creazione" }];
+  sorts: { value: string, label: string }[] = [{ value: "ASC", label: "Crescente" }, { value: "DESC", label: "Decrescente" }];
+  sizes: { value: string, label: string }[] = [{ value: "1", label: "Uno (1)" }, { value: "5", label: "Cinque (5)" },
+  { value: "10", label: "Dieci (10)" }, { value: "20", label: "Venti (20)" }];
+  pages: number[] = [1];
+  constructor(private administrationService: AdministrationService, private giochiService: GiochiService, private changeDet: ChangeDetectorRef) { }
 
+  ngAfterContentChecked(): void {
+    this.changeDet.detectChanges();
+    this.changeDet.markForCheck();
+  }
   ngOnInit(): void {
     this.innerWidth = window.innerWidth;
+    this.initializeForms();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -25,21 +43,35 @@ export class AdGiochiComponent implements OnInit {
     this.innerWidth = window.innerWidth;
   }
 
-  handleAction(action:string){
-    switch(action){
-      case("Supervisiona giochi"):{
 
-      }
-      break;
-      case("Modifica un gioco"):{
+  initializeForms() {
+    this.filterForms = new FormGroup({
+      nome: new FormControl(''),
+      difficolta: new FormControl(''),
+      punteggio: new FormControl(''),
+      categoria: new FormControl(''),
+      orderBy: new FormControl(''),
+      sortOrder: new FormControl(''),
+      items:new FormControl(''),
+      page:new FormControl('')
+    })
+  }
 
-      }
-      break;
-      case("Cancella un gioco"):{
-
-      }
-      break;
-      default:{}
+  searchGiochi() {
+    let body = {
+      nome: this.filterForms.get('nome')?.value,
+      punteggio: this.filterForms.get('punteggio')?.value,
+      difficolta: this.filterForms.get('difficolta')?.value,
+      categoria: this.filterForms.get('categoria')?.value,
     }
+    this.giochiService.searchGiochi(body, this.page, this.size, this.orderBy, this.sortOrder).subscribe({
+      next: (value: any) => {
+        this.giochi = value;
+        this.pages = [];
+        for (let p = 1; p <= this.giochi?.totalPages; p++) {
+          this.pages.push(p);
+        }
+      }
+    })
   }
 }
--- src/services/administration.service.ts
+++ src/services/administration.service.ts
@@ -1,8 +1,30 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from "../core/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AdministrationService {
-    constructor() { }
+    private gioco: string = '/gioco';
+    private byFilters: string = '/byFilter';
+    private giochi: string = '';
+
+    constructor(private http: HttpClient) { }
+
+
+
+    getAll(body: { nome: string, difficolta: number, punteggio: number }, page: number, size: number, orderBy: string, sortOrder: string, isActive?: boolean) {
+
+        let params: HttpParams = new HttpParams();
+        params = params.set('page', page);
+        params = params.set('size', size);
+        params = params.set('orderBy', orderBy);
+        params = params.set('sortOrder', sortOrder);
+        if (body.nome) params = params.set('nomeGioco', body.nome);
+        if (body.difficolta) params = params.set('difficolta', body.difficolta);
+        if (body.punteggio) params = params.set('punteggio', body.punteggio);
+        if(isActive) params = params.set('isActive',isActive);
+        return this.http.get(environment.API_URL + this.gioco + this.byFilters, { params: params })
+    }
 }
