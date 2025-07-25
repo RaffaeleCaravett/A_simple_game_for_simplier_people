5573f176f96fdda5af7fe0e15fff0b483b5f7ad8
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-07-25 05:52:10 +0200

    to fix

--- src/components/lobby/components/chat/chat.component.ts
+++ src/components/lobby/components/chat/chat.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../../../services/auth.service';
 import { User } from '../../../../interfaces/interfaces';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent implements OnInit {
   user: User | null = null;
-
+  windowWidth: number = 0;
   constructor(private activatedRoute: ActivatedRoute, private authService: AuthService) { }
   ngOnInit(): void {
-    console.log(this.activatedRoute.snapshot.queryParams['user']);
+    this.windowWidth = window.innerWidth;
     if (this.activatedRoute.snapshot.queryParams['user'] != null) {
       this.authService.getUserById(Number(JSON.parse(this.activatedRoute.snapshot.queryParams['user']))).
         subscribe({
@@ -28,4 +29,8 @@ export class ChatComponent implements OnInit {
     }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth = window.innerWidth;
+  }
 }
