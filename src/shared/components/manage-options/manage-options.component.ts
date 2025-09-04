b367afb36326bca4fdc39e12ca58d41df1622ba4
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-09-04 17:21:04 +0200

    created components

---
+++ src/shared/components/manage-options/manage-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-options.component.html',
+  styleUrl: './manage-options.component.scss'
+})
+export class ManageOptionsComponent {
+
+}
