616a825ef013a52463a8e6e57d30e7e26f1e8ae0
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-10-07 15:00:57 +0200

    restyled home

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,14 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
-import { AfterViewInit, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { isPlatformBrowser, NgClass, NgFor, NgIf } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { ModeService } from '../../services/mode.service';
 import { browserRefresh } from '../../app/app.component';
+import { ChartModule } from 'primeng/chart';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass],
+  imports: [NgIf, NgFor, NgClass, ChartModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -16,12 +18,27 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   interval: any;
   mode: string = 'light';
   balls: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  constructor(private modeService: ModeService) {
+  data: any;
+  options: any;
+  platformId = inject(PLATFORM_ID);
+  giochi: any = null;
+  stars: number[] = [1, 2, 3, 4, 5];
+  constructor(private modeService: ModeService, private cd: ChangeDetectorRef, private homeService: HomeService) {
     this.modeService.mode.subscribe((data: string) => {
       this.mode = data;
     })
   }
-
+  sizeScroll(event: any, div: HTMLDivElement) {
+    if (event.clientX < this.windowWidth / 2) {
+      if (event.clientX < this.windowWidth / 2 - 300) {
+        div.scrollLeft -= 8;
+      }
+    } else {
+      if (event.clientX > this.windowWidth / 2 + 300) {
+        div.scrollLeft += 8;
+      }
+    }
+  }
   rotate(id: string) {
     let div = document.querySelector(`#${id}`) as HTMLDivElement;
     div.style.transition = '1s';
@@ -33,6 +50,62 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     if (!browserRefresh) {
       localStorage.setItem('location', '')
     }
+    this.initChart();
+    this.getGiochi();
+  }
+
+  getGiochi() {
+    this.homeService.getGiocoPreview().subscribe({
+      next: (datas: any) => {
+        this.giochi = datas;
+      }
+    })
+  }
+  initChart() {
+    this.homeService.getStats().subscribe({
+      next: (data: any) => {
+        if (isPlatformBrowser(this.platformId)) {
+          const documentStyle = getComputedStyle(document.documentElement);
+          const textColor = documentStyle.getPropertyValue('--p-text-color');
+          const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+          this.data = {
+            datasets: [
+              {
+                data: [data?.categoriesCount, data?.userCount, data?.torneiCount, data?.gamesCount],
+                backgroundColor: [
+                  'red',
+                  'orange',
+                  'green',
+                  'lightblue',
+                ],
+                label: 'Un po\' di numeri ...',
+
+              }
+            ],
+            labels: ['Categorie disponibili', 'Utenti registrati', 'Numero di tornei', 'Giochi']
+          };
+
+          this.options = {
+            plugins: {
+              legend: {
+                labels: {
+                  color: textColor
+                }
+              }
+            },
+            scales: {
+              r: {
+                grid: {
+                  color: surfaceBorder
+                }
+              }
+            }
+          };
+          this.cd.markForCheck()
+        }
+      }
+    })
   }
   ngAfterViewInit(): void {
 
--- src/services/home.service.ts
+++ src/services/home.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../core/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  private stats: string = '/stats';
+  private auth: string = '/auth';
+  private giocoPrevies = '/gamePreview';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getStats() {
+    return this.http.get(environment.API_URL + this.auth + this.stats);
+  }
+  getGiocoPreview() {
+    return this.http.get(environment.API_URL + this.auth + this.giocoPrevies);
+  }
 }
