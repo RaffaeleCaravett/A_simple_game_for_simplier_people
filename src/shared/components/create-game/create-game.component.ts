ec37e50042c0885aee3050622f5787aa9a5b7be6
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-10-13 09:16:01 +0200

    doing add game

--- src/shared/components/create-game/create-game.component.ts
+++ src/shared/components/create-game/create-game.component.ts
@@ -1,27 +1,35 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from "@angular/material/dialog";
 import { Categoria } from '../../../interfaces/interfaces';
 import { ToastrService } from 'ngx-toastr';
 import { GiochiService } from '../../../services/giochi.service';
-
+import { Slider } from 'primeng/slider';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-create-game',
   standalone: true,
-  imports: [MatDialogActions, MatDialogTitle, MatDialogContent, ReactiveFormsModule, NgIf, NgFor, NgClass],
+  imports: [MatDialogActions, MatDialogTitle, MatDialogContent, ReactiveFormsModule, NgIf, NgFor, NgClass, FormsModule, Slider, InputTextModule],
   templateUrl: './create-game.component.html',
   styleUrl: './create-game.component.scss'
 })
 export class CreateGameComponent implements OnInit {
+
   aggiungiGiocoForm: FormGroup = new FormGroup({});
   selectedImage: File | null = null;
   imageUrl: string = '';
   categorie: Categoria[] = [];
   choosedCategories: Categoria[] = [];
+  value: number = 0;
+  stars: number[] = [1, 2, 3, 4, 5];
+  difficoltaTouched: boolean = false;
+  step: number = 1;
   constructor(private dialogRef: MatDialogRef<CreateGameComponent>, private toastr: ToastrService, private giochiService: GiochiService
   ) { }
-
+  number(arg0: string) {
+    return Number(arg0);
+  }
   ngOnInit(): void {
     this.initializeForms();
   }
@@ -29,8 +37,7 @@ export class CreateGameComponent implements OnInit {
   initializeForms() {
     this.aggiungiGiocoForm = new FormGroup({
       nomeGioco: new FormControl('', Validators.required),
-      difficolta: new FormControl('', [Validators.required, Validators.min(1), Validators.max(5)]),
-      descrizione: new FormControl('', Validators.required)
+      descrizione: new FormControl('', [Validators.required, Validators.maxLength(120)])
     });
   }
   handleImageEvent(event: any) {
