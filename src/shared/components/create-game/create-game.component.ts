af0332ede00feb0ba78085303aa45fffe911e6f7
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-10-16 09:37:20 +0200

    doing autocomplete

--- src/shared/components/create-game/create-game.component.ts
+++ src/shared/components/create-game/create-game.component.ts
@@ -7,10 +7,14 @@ import { ToastrService } from 'ngx-toastr';
 import { GiochiService } from '../../../services/giochi.service';
 import { Slider } from 'primeng/slider';
 import { InputTextModule } from 'primeng/inputtext';
+import { MatTooltip } from "@angular/material/tooltip";
+import { AutoComplete, AutoCompleteCompleteEvent } from 'primeng/autocomplete';
+import { AdministrationService } from '../../../services/administration.service';
 @Component({
   selector: 'app-create-game',
   standalone: true,
-  imports: [MatDialogActions, MatDialogTitle, MatDialogContent, ReactiveFormsModule, NgIf, NgFor, NgClass, FormsModule, Slider, InputTextModule],
+  imports: [MatDialogActions, MatDialogTitle, MatDialogContent, ReactiveFormsModule, NgIf, NgFor, NgClass, FormsModule,
+    Slider, InputTextModule, MatTooltip, AutoComplete],
   templateUrl: './create-game.component.html',
   styleUrl: './create-game.component.scss'
 })
@@ -20,13 +24,15 @@ export class CreateGameComponent implements OnInit {
   selectedImage: File | null = null;
   imageUrl: string = '';
   categorie: Categoria[] = [];
+  categorieNames: string[] = [];
   choosedCategories: Categoria[] = [];
   value: number = 0;
   stars: number[] = [1, 2, 3, 4, 5];
   difficoltaTouched: boolean = false;
   step: number = 1;
-  constructor(private dialogRef: MatDialogRef<CreateGameComponent>, private toastr: ToastrService, private giochiService: GiochiService
-  ) { }
+  valueCategory: string = "";
+  constructor(private dialogRef: MatDialogRef<CreateGameComponent>, private toastr: ToastrService, private giochiService: GiochiService,
+    private administrationService: AdministrationService) { }
   number(arg0: string) {
     return Number(arg0);
   }
@@ -55,4 +61,13 @@ export class CreateGameComponent implements OnInit {
     this.selectedImage = null;
   }
 
+
+  searchCategories(event: AutoCompleteCompleteEvent) {
+    this.administrationService.getCategoriaByNameContaining(event.query).subscribe({
+      next: (data: any) => {
+        this.categorie = data;
+        this.categorieNames = (data as Categoria[]).slice(0, data?.length > 5 ? 5 : data.length).map(c => c.nome);
+      }
+    });
+  }
 }
